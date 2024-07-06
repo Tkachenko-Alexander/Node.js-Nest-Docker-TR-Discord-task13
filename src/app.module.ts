cbcf4f32d427f9ac2ef902c91a63c9975cdfc871
Aleksandr <mivotanaj@gmail.com>
2024-07-06 12:50:45 +0300

    secondCommitfor Save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
 import { Module } from "@nestjs/common";
-import { AppController } from "./app.controller";
-import { AppService } from "./app.service";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UsersModule } from './users/users.module';
 
 
 @Module({
-    controllers: [AppController],
-    providers: [AppService]
+    controllers: [],
+    providers: [],
+    imports: [
+        TypeOrmModule.forRoot({
+            type: 'postgres',
+            host: 'localhost',
+            port: 5432,
+            username: 'postgres',
+            password: '1234',
+            database: 'nest-course1',
+            entities: [],
+            synchronize: true,
+            autoLoadEntities: true
+          }),
+        UsersModule,
+    ]
 })
 export class AppModule {}
