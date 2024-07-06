a84371435024cc757a07f2901f649171a0b827a2
Aleksandr <mivotanaj@gmail.com>
2024-07-06 20:11:48 +0300

    first save

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,23 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from "@nestjs/config";
 
 
 @Module({
     controllers: [],
     providers: [],
     imports: [
+        ConfigModule.forRoot({
+            envFilePath: `.${process.env.NODE_ENV}.env`
+        }),
         TypeOrmModule.forRoot({
             type: 'postgres',
-            host: 'localhost',
-            port: 5432,
-            username: 'postgres',
-            password: '1234',
-            database: 'nest-course1',
+            host: process.env.POSTGRES_HOST,
+            port: Number(process.env.POSTGRES_PORT),
+            username: process.env.POSTGRES_USER,
+            password: process.env.POSTGRES_PASSWORD,
+            database: process.env.POSTGRES_DB,
             entities: [],
             synchronize: true,
             autoLoadEntities: true
@@ -22,3 +26,42 @@ import { UsersModule } from './users/users.module';
     ]
 })
 export class AppModule {}
+
+
+
+
+
+// import { Module } from '@nestjs/common';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { UsersModule } from './users/users.module';
+// import { ConfigModule, ConfigService } from '@nestjs/config';
+
+
+// @Module({
+//   controllers: [],
+//   providers: [],
+//   imports: [
+//     ConfigModule.forRoot({
+//       isGlobal: true,
+//       envFilePath: `.${process.env.NODE_ENV}.env`, // Если вы используете файлы .env для разных сред
+//     }),
+//     TypeOrmModule.forRootAsync({
+//       imports: [ConfigModule],
+//       useFactory: async (configService: ConfigService) => ({
+//         type: 'postgres',
+//         host: configService.get<string>('POSTGRES_HOST'),
+//         port: configService.get<number>('POSTGRES_PORT'),
+//         username: configService.get<string>('POSTGRES_USER'),
+//         password: String(configService.get<string>('POSTGRES_PASSWORD')),
+//         database: configService.get<string>('POSTGRES_DB'),
+//         autoLoadEntities: true,
+//         synchronize: true,
+//       }),
+//       inject: [ConfigService],
+//     }),
+//     UsersModule,
+//   ],
+// })
+// export class AppModule {}
+
+
---
+++ src/users/users.model.ts
@@ -0,0 +1,16 @@
+// import { Column, Table } from "typeorm";
+// // import {Model} from "typeorm-typescript";
+// @Table({ tableName: 'users' })
+// export class User extends Model<User, > {
+//     @Column({type: DataType.INTEGER, unique: true, autoIncrement: true, primaryKey: true})
+//     id: number;
+
+//     email: string;
+
+//     password: string;
+
+//     banned: boolean;
+
+//     banReason: string;
+
+// }
